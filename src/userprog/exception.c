2d06121c08de5d777d442860b476db69744abec2
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-05-12 19:35:43 +0300

    edited exception.c

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,6 +148,10 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  validate_void_ptr(fault_addr);
+  if(!write || !not_present || !user)
+    sys_exit(-1);
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
@@ -158,4 +162,3 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
-
